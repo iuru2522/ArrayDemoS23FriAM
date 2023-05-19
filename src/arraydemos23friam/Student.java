d0e4c1f860d1d6236fdcc0e574429163608b8702
Iurii Manastyrskyi <manastyr@sheridancollege.ca>
2023-05-19 09:38:21 -0400

    removing phoneNo attribute

--- src/arraydemos23friam/Student.java
+++ src/arraydemos23friam/Student.java
@@ -5,8 +5,6 @@ package arraydemos23friam;
 public class Student {
     private String sId;
     private String sName;
-    
-    private String phoneNo;
 
     public Student(String sId, String sName) {
         //Comment added at 9:27
